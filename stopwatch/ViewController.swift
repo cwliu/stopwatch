5d591461cd97e21705810eb29a0a5e8b364cbd25
taavirehemagi <taavirehemagi@gmail.com>
2016-06-15 12:05:30 +0200

    Basic layout and background colors.

--- stopwatch/ViewController.swift
+++ stopwatch/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    let daycolor = UIColor(red: 241/255.0, green: 207/255.0, blue: 99/255.0, alpha: 1.0)
+    let nightColor = UIColor(red: 31/255.0, green: 30/255.0, blue: 69/255.0, alpha: 1.0)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = daycolor
         // Do any additional setup after loading the view, typically from a nib.
     }
 
